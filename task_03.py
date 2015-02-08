f41fff063af400c8d67a09d95655eee8344410cb
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-02-08 18:37:07 +0000

    Ran pylint testing for task_03.py

--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This is a doctstring"""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
